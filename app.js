56d64b3eb0b8ff4b9919f0922b6aed0007262984
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-09 16:11:52 -0600

    added commands

--- app.js
+++ app.js
@@ -22,3 +22,16 @@ client.on("connected", function (address, port) {
     console.log("Address: " + address + " Port: " + port);
     client.action("justatoolbox", "Hello, I am a bot for justatoolbox");
 });
+
+client.on("chat", function (channel, userstate, message, self) {
+    // Don't listen to my own messages..
+    if (self) return;
+
+    // Do your stuff.
+    if (message == "!twitter") {
+      client.action("justatoolbox", userstate['display-name'] +
+        ", www.twitter.com/sharmaraghav260");
+    }
+    client.action("justatoolbox", userstate['display-name'] +
+      ", you are a total noob.");
+});
